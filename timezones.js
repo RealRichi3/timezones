8ccffeed87435e66601b274f58fd984c99341c43
RealRichi3 <molunorichie@gmail.com>
2022-04-14 04:28:44 +0100

    Changed content in dropdown from countries to timezones

--- timezones.js
+++ timezones.js
@@ -1,20 +1,30 @@
 // const ct = require('countries-and-timezones');
 
-const countries = ct.getAllCountries();
-const cities = document.getElementById('city-list');
-// let sortedCountries = Object.keys(countries).sort()
-// let first = countries[sortedCountries[0]] 
-// console.log(first.name)
-// console.log(countries);
-// console.log()
-const countryList = Object.keys(countries)
-let cityList = document.querySelector('#city-list')
+// const countries = ct.getAllCountries();
+// // const cities = document.getElementById('city-list');
+// let cityName = Object.entries(countries).sort()
+// const countryList = Object.keys(countries)
+// let cityList = document.querySelector('#city-list')
 
-// console.log(cityList)
-for (let i = 0; i < 10; i++) {
+// // // console.log(cityList)
+// for (let i = 0; i < 20; i++) {
+//     let city = document.createElement('option')
+//     city.setAttribute("value", countryList[i])
+//     city.textContent = cityName[i][1].timezones
+//     cityList.appendChild(city)
+// }
+
+
+// console.log(cityName)
+
+let allTimeZones = ct.getAllTimezones();
+const dropdown = document.getElementById('city-list');
+let timeZoneNames = Object.entries(allTimeZones).sort();
+// console.log(timeZoneNames)
+
+for (let i = 0; i < 20; i++) {
     let city = document.createElement('option')
-    city.setAttribute("value", countryList[i])
-    city.textContent = countryList[i]
-    cityList.appendChild(city)
+    city.setAttribute("value", timeZoneNames[0][i])
+    city.textContent = timeZoneNames[i][0]
+    dropdown.appendChild(city)
 }
-
