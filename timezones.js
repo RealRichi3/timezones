b2cd7e529a97e7e92d37e392238be471c2d46e10
RealRichi3 <molunorichie@gmail.com>
2022-04-14 15:36:56 +0100

    Added js module to repo | Added JS code to update middle time box (timezone details) after selection

--- timezones.js
+++ timezones.js
@@ -1,30 +1,29 @@
-// const ct = require('countries-and-timezones');
+// const ct = require('countries-and-timezones/');
 
-// const countries = ct.getAllCountries();
-// // const cities = document.getElementById('city-list');
-// let cityName = Object.entries(countries).sort()
-// const countryList = Object.keys(countries)
-// let cityList = document.querySelector('#city-list')
-
-// // // console.log(cityList)
-// for (let i = 0; i < 20; i++) {
-//     let city = document.createElement('option')
-//     city.setAttribute("value", countryList[i])
-//     city.textContent = cityName[i][1].timezones
-//     cityList.appendChild(city)
-// }
-
-
-// console.log(cityName)
 
 let allTimeZones = ct.getAllTimezones();
 const dropdown = document.getElementById('city-list');
 let timeZoneNames = Object.entries(allTimeZones).sort();
-// console.log(timeZoneNames)
 
 for (let i = 0; i < 20; i++) {
-    let city = document.createElement('option')
+    window.city = document.createElement('option')
     city.setAttribute("value", timeZoneNames[0][i])
     city.textContent = timeZoneNames[i][0]
     dropdown.appendChild(city)
+}
+
+
+// Listen for selected Timezone in dropdown menu
+dropdown.addEventListener('change', function() {
+    let selectedTimeZone = dropdown.options[dropdown.selectedIndex].text.split("/");
+    continentZone = selectedTimeZone[0]
+    cityZone = selectedTimeZone[1]
+    updateTextContents(cityZone, continentZone)
+
+  });
+
+// Updates textcontent for country details in mid time-box
+function updateTextContents(city, continent){
+    document.querySelector(".mid-pane .city").textContent = city
+    document.querySelector(".mid-pane .country").textContent = continent
 }
