b7f9893eca4a46515c7c868e8fdf0727a3fcd3b3
RealRichi3 <Molunorichie@gmail.com>
2022-05-01 19:47:58 +0100

    Cleaned the code

--- timezones.js
+++ timezones.js
@@ -73,6 +73,21 @@ function updateTime(selector, time){
 }
 
 
+// To generate time and timezone for other boxes
+function generateSmallTimezones(){
+    let arrayTimeZones = ["Europe/Paris", "Europe/London", "America/New_York", "Asia/Tokyo"]
+    let otherTimeBoxes = document.getElementsByClassName('othercity')
+
+    for (let step = 0; step < arrayTimeZones.length; step++) {
+        let idValue = otherTimeBoxes[step].attributes[1].nodeValue
+        let timeZone = arrayTimeZones[step].split('/')
+        let timeZoneAttr = ct.getTimezone(arrayTimeZones[step])
+        
+        updateTimeBox(timeZoneAttr, timeZone, `#${idValue}`)
+    }
+}
+
+// Updates the timebox with the city, continent, and time
 function updateTimeBox(timeZoneAttr, timeZone, classOrIdName){
     let utcOffsetArray = timeZoneAttr.utcOffsetStr.split(':')      
     let offsetHour = parseInt(utcOffsetArray[0]) + checkDayLightSaving(timeZoneAttr)
@@ -80,56 +95,18 @@ function updateTimeBox(timeZoneAttr, timeZone, classOrIdName){
     
     updateTextContents(`${classOrIdName}`, timeZone[1], timeZone[0])
     updateTime(`${classOrIdName} .digi-time`, calcTime(offsetHour, offsetMin))
-
-    // generateSmallTimezones() 
-}
-
-// To generate time and timezone for other boxes
-function generateSmallTimezones(){
-    let arrayTimeZones = ["Europe/Paris", "Europe/London", "America/New_York", "Asia/Tokyo"]
-    let otherTimeBoxes = document.getElementsByClassName('othercity')
-
-    for (let step = 0; step < arrayTimeZones.length; step++) {
-        // cityAndContinent = arrayTimeZones[step].split('/')
-        let idValue = otherTimeBoxes[step].attributes[1].nodeValue
-        let timeZoneAttributes = ct.getTimezone(arrayTimeZones[step])
-        let utcOffsetString = timeZoneAttributes.utcOffsetStr.split(':')
-    
-        // let hour = parseInt(utcOffsetString[0]) + checkDayLightSaving(timeZoneAttributes)
-        // let min = parseInt(utcOffsetString[1])
-
-        let timeZone = arrayTimeZones[step].split('/')
-        let timeZoneAttr = ct.getTimezone(arrayTimeZones[step])
-        
-        updateTimeBox(timeZoneAttr, timeZone, `#${idValue}`)
-
-        
-        // updateTextContents(`#${idValue}`, cityAndContinent[1], cityAndContinent[0])
-        // updateTime(`#${idValue} .digi-time`, calcTime(hour, min))
-    }
 }
 
 // Listen for selected timezone in dropdown menu and updates text content(time and timezone)
 dropdown.addEventListener('change', function() {
     let selectedTimeZone = dropdown.options[dropdown.selectedIndex].text
-    let splitSelectedTimeZone = selectedTimeZone.split("/");
-
     let timeZone = selectedTimeZone.split("/")
     let timeZoneAttr = ct.getTimezone(selectedTimeZone)
     
     updateTimeBox(timeZoneAttr, timeZone, '#third-city')
-
-    // let utcOffsetArray = ct.getTimezone(selectedTimeZone).utcOffsetStr.split(':')      
-    // let offsetHour = parseInt(utcOffsetArray[0]) + checkDayLightSaving(ct.getTimezone(selectedTimeZone))
-    // let offsetMin = parseInt(utcOffsetArray[1])
-    
-    // updateTextContents('#third-city', splitSelectedTimeZone[1], splitSelectedTimeZone[0])
-    // updateTime('#third-city .digi-time', calcTime(offsetHour, offsetMin))
-
     generateSmallTimezones() 
   })
 
 generateSmallTimezones()    
 updateTime(`#mid`, calcTime(+1, 00))            // Set default time, corresponds with Africa/Lagos timezone
 
-console.log(ct)
